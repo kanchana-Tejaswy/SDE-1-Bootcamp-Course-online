24984ba5e1ae539943409ae5e61f94732761b76c
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-16 23:01:53 +0530

    added file code writen for introduction for java explaining about main file structure of java program

---
+++ java/bro code 12hrs course/introduction/newline.java
@@ -0,0 +1,19 @@
+public class newline {
+    public static void main(String[] args) {
+        System.out.print("hello");// print is used to print message 
+        System.out.println("       my name is teja ");// println is used to print mwssage in new line
+        System.out.print(" \n hii"); // \n is used to print message in new line 
+        
+        
+        /*   
+          
+        // = this symbol is used to write a single line comment 
+        and  / stat  star / is used to write comments in multiple line 
+        comments are used to write logic in the form of message so that anoter programer can understand logic 
+    */ 
+        
+        
+        
+        
+    }
+}
