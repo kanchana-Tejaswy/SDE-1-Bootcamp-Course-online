1a25bc34a4df71a764d50e4e37dd7858efcd9682
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-17 21:23:38 +0530

    writen code for double dsata type

---
+++ java/bro code 12hrs course/variable/douFun.java
@@ -0,0 +1,15 @@
+
+public class douFun {
+    public static void main(String[] args) {
+        
+    double price = 19.99;
+    double gpa = 3.8;
+    double temperature = -12.6;
+    System.out.println("prize: " + price);
+    System.out.println("GPA: " + gpa);  
+    System.out.println("temperature: " + temperature);
+
+  
+    }
+    
+}
