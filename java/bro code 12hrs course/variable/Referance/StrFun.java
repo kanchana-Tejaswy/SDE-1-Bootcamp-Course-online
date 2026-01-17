052b0c12865eb7717caf2f3a927a6e15baf69f02
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-17 21:57:19 +0530

    added file for referance and string data type with edeited and exicuted code

--- java/bro code 12hrs course/variable/Referance/StrFun.java
+++ java/bro code 12hrs course/variable/Referance/StrFun.java
@@ -1,13 +1,17 @@
 
-public class StrFun {
+public class StrFun{
     public static void main(String[] args) {
         
     
-    String name = "John Doe";
-    String address = "123 Main St, Anytown, USA";
+    String name = "tejaswy";
+    String address = "123 Main St, hyd, india";
     String email = "mail2tejaswy@gmail.com";
 
     System.out.println("Name: " + name);
     System.out.println("Address: " + address);
     System.out.println("Email: " + email);
 }
+}
+/* Name: tejaswy
+Address: 123 Main St, hyd, india
+Email: mail2tejaswy@gmail.com  */
