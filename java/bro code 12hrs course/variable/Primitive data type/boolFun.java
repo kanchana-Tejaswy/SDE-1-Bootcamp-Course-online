6ba22f01018724e17a315425284d0a332dd61231
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-17 21:44:17 +0530

    writen code for bollean value

---
+++ java/bro code 12hrs course/variable/Primitive data type/boolFun.java
@@ -0,0 +1,28 @@
+
+
+public class boolFun {
+    public static void main(String[] args) {
+        
+    
+    boolean isStudent = true;
+    boolean isForSale = false;
+    boolean isOnline = true;  
+    
+    if(isStudent){
+        System.out.println("You are a student");
+    } else {
+        System.out.println("You are not a student");
+    }
+    if(isForSale){
+        System.out.println("The item is for sale");
+    } else {
+        System.out.println("The item is not for sale");
+    }
+    if(isOnline){
+        System.out.println("You are online");
+    } else {
+        System.out.println("You are offline");
+    }
+
+}
+}
---
+++ java/bro code 12hrs course/variable/Primitive data type/charFun.java
@@ -0,0 +1,12 @@
+public class charFun {
+    public static void main(String[] args) {
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println("Grade: " + grade);
+        System.out.println("Symbol: " + symbol);        
+        System.out.println("Currency: " + currency);
+    }
+    
+}
