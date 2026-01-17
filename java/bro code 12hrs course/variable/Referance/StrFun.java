01c265fb2f3ac881fabc890a0508e1f225dc46d2
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-17 21:51:59 +0530

    added file for referance and string data type

---
+++ java/bro code 12hrs course/variable/Referance/StrFun.java
@@ -0,0 +1,9 @@
+
+public class StrFun {
+    public static void main(String[] args) {
+        
+    
+    String name = "John Doe";
+    String address = "123 Main St, Anytown, USA";
+    String email = "
+}
