131cd1597195d9688505028765534e64349c3783
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-20 21:06:26 +0530

    writen code for increment and decrement

---
+++ java/bro code 12hrs course/Arthematic/IncDecOp.java
@@ -0,0 +1,15 @@
+public class IncDecOp {
+
+public static void main(String[] args) {
+    // Increment and Decrement Operation 
+
+    int x = 1;
+
+    x++;
+    System.out.println(x);  // 2
+     x--;
+    System.out.println(x);  // 1
+
+    }
+    
+}
