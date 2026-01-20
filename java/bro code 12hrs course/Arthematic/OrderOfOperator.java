0063e4ee0fc5d93bcd80532171e7a31216a6a669
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-20 21:21:14 +0530

    writen code for order of operation

---
+++ java/bro code 12hrs course/Arthematic/OrderOfOperator.java
@@ -0,0 +1,9 @@
+public class OrderOfOperator {
+    public static void main(String[] args) {
+        // ORDER OF OPERATION [ R-E-M-D-A-S]
+
+        double result = 3 + 4 * ( 7 - 5) / 2.0;
+
+        System.out.println(result);
+    }
+}
