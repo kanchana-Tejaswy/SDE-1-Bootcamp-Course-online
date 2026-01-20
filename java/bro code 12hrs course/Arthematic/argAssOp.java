4b94bc4a43f5430c74ff57afcd6e590e9a64f144
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-20 20:58:02 +0530

    writen code for argument assigenment operator

---
+++ java/bro code 12hrs course/Arthematic/argAssOp.java
@@ -0,0 +1,22 @@
+public class argAssOp {
+    public static void main(String[] args) {
+
+        // Augmented Assigrnment Operation
+
+        int x = 10;
+        int y = 3;
+
+        x = x + y;
+        System.out.println(x);  // 13
+
+         x += y;
+        System.out.println(x);  // 16
+
+        x -= y;
+        System.out.println(x);  //  13
+
+         x *= y;
+        System.out.println(x);  // 39
+
+    }
+}
